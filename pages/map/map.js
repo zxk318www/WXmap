4f6e73317899aefeff7cf782acedc30ba4fdd535
Zhangsnke <zhangxike@wondersgroup.com>
2018-12-14 16:45:59 +0800

    添加计算道路规划，添加距离计算
    包括开车，骑行，步行等道路规划

--- pages/map/map.js
+++ pages/map/map.js
@@ -11,10 +11,15 @@ Page({
     speed: 0,
     accuracy: 0,
     markers: [],
+    polyline: [],
 
     inputShowed: false,
     inputVal: "",
-    searchResults: [] //搜索框提示信息
+    searchResults: [], //搜索框提示信息
+
+    toLat: 0, //目的地的维度
+    toLng: 0, //目的地的经度
+    distance: 0 //目的地距离,单位米
   },
   //事件处理函数
   bindViewTap: function () {
@@ -93,6 +98,10 @@ Page({
   },
   search(param) {
     var that = this
+    this.setData({
+      markers: [],
+      polyline:[]
+    })
     qqmapsdk.search({
       keyword: param,
       success: function (res) {
@@ -108,26 +117,22 @@ Page({
             width: 20,
             height: 20,
             callout: {
-              content: res.data[i].title +'\n'+ '('+res.data[i].address+')' || '',
+              content: res.data[i].title + '\n' + '(' + res.data[i].address + ')' || '',
               fontSize: 12,
               bgColor: "#FFF",
               borderWidth: 1,
               borderColor: "#CCC",
               padding: 4,
-             
+
               textAlign: "center"
             }
           })
         }
         that.setData({
           markers: mks
+          
         })
-      },
-      fail: function (res) {
-        console.log(res);
-      },
-      complete: function (res) {
-        //console.log(res);
+       
       }
     })
   },
@@ -138,7 +143,7 @@ Page({
     qqmapsdk.getSuggestion({
       keyword: param,
       success: function (res) {
-        console.log(res)
+        // console.log(res)
         that.setData({
           searchResults: res.data
         })
@@ -150,9 +155,10 @@ Page({
     this.search(e.currentTarget.dataset.title);
   },
   select(e) {
-    console.log(e.currentTarget.dataset.object);
+    //console.log(e.currentTarget.dataset.object);
     this.setData({
-      markers: []
+      markers: [],
+      polyline:[]
     })
     var obj = e.currentTarget.dataset.object;
     let mks = []
@@ -165,7 +171,7 @@ Page({
       width: 20,
       height: 20,
       callout: {
-        content: obj.title +'\n'+ '('+obj.address+')' || '',
+        content: obj.title + '\n' + '(' + obj.address + ')' || '',
         fontSize: 12,
         bgColor: "#FFF",
         borderWidth: 1,
@@ -176,21 +182,26 @@ Page({
       }
     }
     mks.push(selmk);
-    console.log(mks);
+    //console.log(mks);
     this.setData({
-      markers: mks
+      markers: mks,
+      toLat:selmk.latitude,
+      toLng:selmk.longitude
     })
+    this.jsJL(obj.location.lat, obj.location.lng);
     this.hideInput();
   },
+  //附近
   getPoint() {
     var that = this
     that.setData({
-      markers: []
+      markers: [],
+      polyline:[]
     })
     wx.chooseLocation({
 
       success: function (res) {
-        console.log(res)
+        //console.log(res)
         let mks = []
         var selmk = {
           title: res.name,
@@ -202,13 +213,13 @@ Page({
           height: 20,
 
           callout: {
-            content: res.name +'\n'+ '('+res.address+')' || '',
+            content: res.name + '\n' + '(' + res.address + ')' || '',
             fontSize: 12,
             bgColor: "#FFF",
             borderWidth: 1,
             borderColor: "#CCC",
             padding: 4,
-            display: "ALWAYS",
+
             textAlign: "center"
           }
         }
@@ -217,6 +228,9 @@ Page({
         that.setData({
           markers: mks
         })
+        that.jsJL(res.latitude, res.longitude);
+      }, complete() {
+
       }
 
     })
@@ -252,7 +266,97 @@ Page({
       }
     })
   },
-  tapMark() {
-    console.log("chumo")
+  tapMark(e) {
+    console.log(e)
+    var mks = this.data.markers
+    console.log(mks)
+    var that = this
+    for(var i=0;i<mks.length;i++){
+      if(mks[i].id==e.markerId){
+        that.setData({
+          toLat:mks[i].latitude,
+          toLng:mks[i].longitude
+        })
+        that.jsJL(mks[i].latitude, mks[i].longitude);
+      }
+    }
+    
+  },
+  //计算距离
+  jsJL(lat, lng) {
+    var that = this
+    qqmapsdk.calculateDistance({
+      to: [{
+        latitude: lat,
+        longitude: lng
+      }],
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          toLat: res.result.elements[0].to.lat,
+          toLng: res.result.elements[0].to.lng,
+          distance: res.result.elements[0].distance
+        })
+      }
+    })
+  },
+  //规划路线
+  guihua(e) {
+    var that = this
+    this.setData({
+      polyline:[]
+    })
+    console.log(e.currentTarget.dataset.type)
+    var type = e.currentTarget.dataset.type
+    var loclat = this.data.latitude
+    var loclng = this.data.longitude
+    var tolat = this.data.toLat
+    var tolng = this.data.toLng
+    var url = ""
+    if (type == 3) {
+      url = "https://apis.map.qq.com/ws/direction/v1/driving/"
+    }
+    if (type == 2) {
+      url = "https://apis.map.qq.com/ws/direction/v1/bicycling/"
+    }
+    if (type == 1) {
+      url = "https://apis.map.qq.com/ws/direction/v1/walking/"
+    }
+    wx.request({
+      url: `${url}?from=${loclat},${loclng}&to=${tolat},${tolng}&key=WFUBZ-OEVC5-APXIL-QQKAH-7NFZE-3WFYU`,
+      data: {},
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        // success
+        console.log(res)
+        var ret = res.data
+        if (ret.status != 0) return; //服务异常处理
+        var coors = ret.result.routes[0].polyline, pl = [];
+        //坐标解压（返回的点串坐标，通过前向差分进行压缩）
+        var kr = 1000000;
+        for (var i = 2; i < coors.length; i++) {
+          coors[i] = Number(coors[i - 2]) + Number(coors[i]) / kr;
+        }
+        //将解压后的坐标放入点串数组pl中
+        for (var i = 0; i < coors.length; i += 2) {
+          pl.push({ latitude: coors[i], longitude: coors[i + 1] })
+        }
+        //设置polyline属性，将路线显示出来
+        that.setData({
+          polyline: [{
+            points: pl,
+            color: '#FF0000DD',
+            width: 2
+          }]
+        })
+      },
+      fail: function () {
+        // fail
+      },
+      complete: function () {
+        // complete
+      }
+    })
   }
 })
